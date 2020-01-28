0be1184a31bba16fd03a3cf0ce0a378e12c8ab65
Carlos <cmartluque@gmail.com>
2020-01-29 00:19:43 +0100

    Implementatio of agendaService and tierService

--- core/src/main/java/poc/hexagonal/port/in/impl/AgendaServiceImpl.java
+++ core/src/main/java/poc/hexagonal/port/in/impl/AgendaServiceImpl.java
@@ -1,4 +1,36 @@
 package poc.hexagonal.port.in.impl;
 
-public class AgendaService {
+import lombok.RequiredArgsConstructor;
+import poc.hexagonal.domain.Agenda;
+import poc.hexagonal.domain.Tier;
+import poc.hexagonal.error.BusinessError;
+import poc.hexagonal.port.in.AgendaService;
+import poc.hexagonal.port.out.AgendaRepository;
+import poc.hexagonal.port.out.TierRepository;
+
+import javax.validation.constraints.NotNull;
+
+@RequiredArgsConstructor
+public class AgendaServiceImpl implements AgendaService {
+    @NotNull
+    private AgendaRepository agendaRepository;
+    @NotNull
+    private TierRepository tierRepository;
+
+    @Override
+    public void addTier(Long agendaId, Long tierId) {
+        final Agenda agenda = findAgendaById(agendaId);
+        final Tier tier = findTierById(tierId);
+
+        agenda.add(tier);
+        agendaRepository.update(agenda);
+    }
+
+    private Agenda findAgendaById(Long id) {
+        return agendaRepository.findById(id).orElseThrow(BusinessError::new);
+    }
+
+    private Tier findTierById(Long id) {
+        return tierRepository.findById(id).orElseThrow(BusinessError::new);
+    }
 }
--- core/src/main/java/poc/hexagonal/port/in/impl/TierServiceImpl.java
+++ core/src/main/java/poc/hexagonal/port/in/impl/TierServiceImpl.java
@@ -1,4 +1,35 @@
 package poc.hexagonal.port.in.impl;
 
-public class TierServiceImpl {
+import lombok.RequiredArgsConstructor;
+import poc.hexagonal.domain.Event;
+import poc.hexagonal.domain.Tier;
+import poc.hexagonal.error.BusinessError;
+import poc.hexagonal.port.in.TierService;
+import poc.hexagonal.port.out.EventRepository;
+import poc.hexagonal.port.out.TierRepository;
+
+import javax.validation.constraints.NotNull;
+
+@RequiredArgsConstructor
+public class TierServiceImpl implements TierService {
+    @NotNull
+    private TierRepository tierRepository;
+    @NotNull
+    private EventRepository eventRepository;
+
+    @Override
+    public void addEvent(Long tierId, Long eventId) {
+        final Tier tier = findTierById(tierId);
+        final Event event = findEventById(eventId);
+        tier.add(event);
+        tierRepository.update(tier);
+    }
+
+    private Tier findTierById(Long id){
+        return tierRepository.findById(id).orElseThrow(BusinessError::new);
+    }
+
+    private Event findEventById(Long id){
+        return eventRepository.findById(id).orElseThrow(BusinessError::new);
+    }
 }
